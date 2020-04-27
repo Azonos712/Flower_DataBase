d8e4b2ccd216ff04ab3c1792bb058a839a3f2b2a
Nikolay <nekooolay@mail.ru>
2020-04-27 13:17:50 +0300

    Иконка приложение, проверки при входе, оповещение роли при входе

--- FlowerClient/Login.xaml.cs
+++ FlowerClient/Login.xaml.cs
@@ -1,23 +1,9 @@
 ﻿using Npgsql;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace FlowerClient
 {
-    /// <summary>
-    /// Логика взаимодействия для Login.xaml
-    /// </summary>
     public partial class Login : Window
     {
         public Login()
@@ -29,13 +15,19 @@ namespace FlowerClient
         {
             try
             {
+                if (String.IsNullOrWhiteSpace(txt_login.Text) || String.IsNullOrWhiteSpace(txt_password.Password) || String.IsNullOrWhiteSpace(txt_adress.Text))
+                {
+                    throw new Exception("Вы заполнили не все поля!");
+                }
+
                 TryConnection();
+                RoleAlert();
                 new MainWindow().Show();
                 this.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                new MsgBox(ex.Message, "Ошибка").ShowDialog();
             }
         }
 
@@ -43,13 +35,17 @@ namespace FlowerClient
         {
             string connectionString = "Server=" + txt_adress.Text.Trim() + ";Port=5432;User Id=" + txt_login.Text.Trim()
             + ";Password=" + txt_password.Password.Trim() + ";Database=flower;";
+
             Mediator.instance.Connection = new NpgsqlConnection(connectionString);
             Mediator.instance.Connection.Open();
 
             Mediator.instance.Login = txt_login.Text.Trim();
             Mediator.instance.SQL = "select show_role('" + Mediator.instance.Login + "');";
             Mediator.instance.Role = (string)Mediator.instance.ConvertQueryToValue();
+        }
 
+        void RoleAlert()
+        {
             string temp_str = String.Empty;
             switch (Mediator.instance.Role)
             {
@@ -63,8 +59,7 @@ namespace FlowerClient
                     temp_str = "Не определён";
                     break;
             }
-            new MsgBox("Вы вошли в систему!(" + temp_str + ")","Оповещение").ShowDialog();
-            //MessageBox.Show("Вы вошли в систему!(" + temp_str + ")");
+            new MsgBox("Вы вошли в систему!(" + temp_str + ")", "Оповещение").ShowDialog();
         }
     }
 }
