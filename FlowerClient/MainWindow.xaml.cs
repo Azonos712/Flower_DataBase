1302594b2da880c91e7e66dbdffa7071cd5ad8e7
Morningstarr <36194870+Morningstarr@users.noreply.github.com>
2020-04-29 18:32:50 +0300

    Карточки. Продолжение

--- FlowerClient/MainWindow.xaml.cs
+++ FlowerClient/MainWindow.xaml.cs
@@ -27,5 +27,16 @@ namespace FlowerClient
         {
             new AdminWindow().ShowDialog();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Card c = new Card();
+            c.captionP = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+            c.nameP = "Flower";
+            c.imageP = @"C:\\Users\\kiril\\Desktop\\1.jpg";
+
+            card1.DataContext = c;
+        }
     }
 }
